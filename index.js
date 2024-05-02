892ad7fc34da32ea3ebcb14c50575b38ec3b7c4b
Reshma2996 <reshuamisha@gmail.com>
2024-05-02 13:51:56 +0530

    connection to mongodb feature

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //connect to the DB
+//import mongoose
+const mongoose = require ( 'mongoose');
+
+//import config module
+const config = require ('./utils/config');
+
+//console.log the message connecting to MongoDB followed by the URI without the password
+console.log('Connecting to MongoDB..')
+//connect to DB
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:' , error.message);
+  });
+
